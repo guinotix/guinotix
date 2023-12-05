1934398d118514b4b97ee988c72cdde24e812457
Alejandro Guinot <al375927@uji.es>
2023-12-05 11:50:09 +0100

    add alpinejs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
+import alpine from '@astrojs/alpinejs';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
     integrations: [
+        alpine(),
         tailwind(),
     ],
 });
