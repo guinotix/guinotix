d2281432d0ae398ca2d10903abe1da72713973c7
Alejandro Guinot <al375927@uji.es>
2025-07-29 15:50:45 +0200

    remove dead code and adjust text size

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,9 @@ import defaultTheme from 'tailwindcss/defaultTheme'
 
 /** @type {import('tailwindcss').Config} */
 export default {
-    darkMode: 'class',
     content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
     theme: {
         extend: {
-            backgroundImage: {
-				'sunset': "url('/sunset.jpg')",
-                'sailaway': "url('/sailaway/sailaway_banner.webp')"
-			},
             fontFamily: {
                 sans: ['Reddit Mono Variable', ...defaultTheme.fontFamily.sans]
             },
