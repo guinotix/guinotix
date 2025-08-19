be4eb65a159a8630569489d14f1157cf5ceb5a6d
Alejandro Guinot <al375927@uji.es>
2025-08-20 01:46:23 +0200

    add i18n features and new project

---
+++ src/i18n/ui.ts
@@ -0,0 +1,49 @@
+export const languages = {
+	en: 'English',
+	es: 'Español',
+};
+
+export const defaultLang = 'es';
+
+export const ui = {
+	en: {
+		'nav.home': 'Home',
+		'nav.about': 'About',
+		'nav.technologies': 'Techonologies',
+		'nav.projects': 'Projects',
+		'footer.crafted': 'Crafted with Astro by ',
+		'index.role': 'Junior Software Engineer',
+		'index.aboutme': 'About me',
+		'index.techs': 'Technologies',
+		'index.projects.title': 'Projects',
+		'index.projects.description': 'Here I showcase some of the projects I have worked in:',
+		'index.projects.moreinfo': 'More info...',
+		'index.projects.techs': 'Techonologies used:',
+		'index.projects.cncep.title': '1st International Congress of Psychology Students (XVIII CNCEP UV)',
+		'index.projects.cncep.description': 'My most recent project! This is a landing page for an international charity conference done by the CEP-PIE (Colectivo de Estudiantes de Psicología). The CEP-PIE is the student representative body in which representatives of psychology students from the different centers of the Spanish university system participate, and I am being involved in the Department of Digital Spaces managing everything about the web, and one of the things I am proud of is helping with the building of this site from the foundation.',
+		'index.projects.mbp.title': 'Music Band Page',
+		'index.projects.mbp.description': "This project is a full-stack web application I developed as a serious challenge. After Sailaway I decided to start one with more ambition for my future as a software developer. Music is one of my hobbies and I spend time looking for some records online so, why not building a project related to this? That's why I came with the idea of building an E-commerce platform powered with some functionalities and styles based on bands that I follow.",
+		'index.projects.sailaway.title': 'Sailaway',
+		'index.projects.sailaway.description': "Sailaway is a nearly identical adaptation of my final degree project, which I have done in 2023. Due to implementation reasons of that project, the original one is not visible, and that's why I made a replica as faithful as possible, omitting things like brand image and some special features."
+	},
+	es: {
+		'nav.home': 'Inicio',
+		'nav.about': 'Sobre mí',
+		'nav.technologies': 'Tecnologías',
+		'nav.projects': 'Proyectos',
+		'footer.crafted': 'Hecho con Astro por ',
+		'index.role': 'Ingeniero de Software Junior',
+		'index.aboutme': 'Sobre mí',
+		'index.techs': 'Tecnologías',
+		'index.projects.title': 'Proyectos',
+		'index.projects.description': 'Aquí muestro algunos de los proyectos en los que he trabajado:',
+		'index.projects.moreinfo': 'Más info...',
+		'index.projects.techs': 'Tecnologías usadas:',
+		'index.projects.cncep.title': 'I Congreso Internacional del Estudiantado de Psicología (XVIII CNCEP UV)',
+		'index.projects.cncep.description': '¡Mi proyecto más reciente! Esta es una Landing Page para un congreso benéfico intenracional organizado por el CEP-PIE (Colectivo de Estudiantes de Psicología). El CEP-PIE es el órgano de representación estudiantil en el que participan representantes de estudiantes de psicología de los diferentes centros del sistema universitario español. Estoy involucrado en el Departamento de Espacios Digitales, gestionando todo lo relacionado con la web. Me siento orgulloso de haber contribuido a la creación de este sitio desde el inicio.',
+		'index.projects.mbp.title': 'Music Band Page',
+		'index.projects.mbp.description': "Este proyecto es una aplicación web full-stack que desarrollé como un desafío real. Después de Sailaway, decidí empezar un proyecto con más ambición para mi futuro como desarrollador de software. Uno de mis hobbies es la música y suelo emplear tiempo en buscar discos online así que, ¿por qué no hacer un proyecto al respecto? Así es como llegué a la idea de construir una plataforma de comercio electrónico con algunas funcionalidades y estilos basados en las bandas que sigo.",
+		'index.projects.sailaway.title': 'Sailaway',
+		'index.projects.sailaway.description': "Sailaway es una adaptación casi idéntica de mi trabajo de final de grado hecho en 2023. Debido a razones de implementación del proyecto, el original no está visible, con lo que hice una réplica lo más fiel posible, omitiendo cosas como imagen de marca y funcionalidades especiales."
+	},
+} as const;
