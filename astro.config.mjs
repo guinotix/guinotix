64622f05c214850a8702f6d26e3e8aaa3766de0c
Alejandro Guinot <al375927@uji.es>
2026-04-20 12:05:01 +0200

    install tailwind 4 and add previous fontsource

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,19 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 import alpinejs from '@astrojs/alpinejs';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
     integrations: [
         alpinejs()
     ],
+    fonts: [{
+        provider: fontProviders.fontsource(),
+        name: "Reddit Mono",
+        cssVariable: "--font-reddit",
+        fallbacks: ["monospace"]
+    }],
+    vite: {
+        plugins: [tailwindcss()],
+    },
 });
